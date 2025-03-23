aa484ad7dacb65521a0c528343abdfb95f1fab4a
NongHoi <nonghoi2004@gmail.com>
2025-03-23 16:03:21 +0700

    Hoi-fix-color

--- ShoeShopWebsite/Models/Product.cs
+++ ShoeShopWebsite/Models/Product.cs
@@ -22,10 +22,6 @@ namespace ShoeShopWebsite.Models
 
         public ICollection<ProductSize> ProductSizes { get; set; } = new List<ProductSize>();
         public ICollection<ProductImage> ProductImages { get; set; } = new List<ProductImage>();
-        public ICollection<ProductColor> ProductColors { get; set; }
-
-
-        //public ICollection<ProductImage> ProductImages { get; set; }
-        //public ICollection<ProductSize> ProductSizes { get; set; }
+        public ICollection<ProductColor> ProductColors { get; set; } = new List<ProductColor>();
     }
 }
